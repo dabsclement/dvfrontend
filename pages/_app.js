8b597c54985c67ba36be72b8e6b53a7b5ec6f607
quadri salawudeen <olamiquadri1@gmail.com>
2022-07-07 17:57:36 +0100

    almost done

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import PProperties from "../components/properties";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <PProperties>
       <Component {...pageProps} />
-    </>
+    </PProperties>
   );
 }
 
--- pages/newpod.js
+++ pages/newpod.js
@@ -9,19 +9,78 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import {
   MdOutlineFormatListBulleted,
   MdOutlineFormatListNumbered,
+  MdOutlineCancel,
 } from "react-icons/md";
 import { useState } from "react";
+// useRef, useEffect;
 
 const newPod = () => {
   const [open, setOpen] = useState(false);
-  const [sty, setSty] = useState(false);
+  const [properties, setProperties] = useState({
+    isBold: false,
+    isItalic: false,
+    isQuote: false,
+    isLink: false,
+    isBullet: false,
+    isList: false,
+    isPic: false,
+    isCode: false,
+    isBraket: false,
+  });
+
   const change = () => {
     setOpen((prev) => !prev);
   };
 
-  const on = (e) => {
-    setSty(e.target?.id);
+  const toggleBold = () => {
+    setProperties((prev) => {
+      return { ...prev, isBold: !prev.isBold };
+    });
   };
+
+  const toggleItalic = () => {
+    setProperties((prev) => {
+      return { ...prev, isItalic: !prev.isItalic };
+    });
+  };
+  const toggleQuote = () => {
+    setProperties((prev) => {
+      return { ...prev, isQuote: !prev.isQuote };
+    });
+  };
+  const toogleLink = () => {
+    setProperties((prev) => {
+      return { ...prev, isLink: !prev.isLink };
+    });
+  };
+  const bullet = () => {
+    setProperties((prev) => {
+      return { ...prev, isBullet: !prev.isBullet };
+    });
+  };
+  const toggleList = () => {
+    setProperties((prev) => {
+      return { ...prev, isList: !prev.isList };
+    });
+  };
+  const pic = () => {
+    setProperties((prev) => {
+      return { ...prev, isPic: !prev.isPic };
+    });
+  };
+  const Code = () => {
+    setProperties((prev) => {
+      return { ...prev, isCode: !prev.isCode };
+    });
+  };
+  const Bracket = () => {
+    setProperties((prev) => {
+      return { ...prev, isBraket: !prev.isBraket };
+    });
+  };
+
+  // image selete and preview
+  // const [images, setImage] = useState();
 
   return (
     <div className="main">
@@ -29,84 +88,98 @@ const newPod = () => {
         <label> Logo</label>
         <button>Preview</button>
       </div>
-      <div className="mid">
-        <div className="check">
-          <button>
-            <FaRegEye />
-            Preview
-          </button>
-          <button>
-            <AiFillPicture />
-            Set Cover
-          </button>
-          <button>
-            <BiSquare />
-            Feature
-          </button>
-        </div>
-        <div className="log">
-          <input type="text" placeholder="Enter Tag" />
-        </div>
-      </div>
-      <div className="content">
-        <input type="text" placeholder="Title..." />
+      <div className="cent">
+        <div className="mid">
+          <div className="check">
+            <button>
+              <FaRegEye />
+              Preview
+            </button>
+            <button>
+              <AiFillPicture />
+              Set Cover
+            </button>
 
-        <textarea cols="30" placeholder="Description"></textarea>
-        <div className="ccancel" onClick={change}>
-          {open ? (
-            <FaTimes className="icon" />
-          ) : (
-            <GiHamburgerMenu className="icon" />
-          )}
+            <button>
+              <BiSquare />
+              Feature
+            </button>
+          </div>
+          <div className="log">
+            <input type="text" placeholder="Enter Tag" />
+          </div>
         </div>
-        <div className={open ? "active" : "het"}>
-          <BiBold
-            id="1"
-            onClick={on}
-            className={sty === "1" ? "cli" : "icon"}
-          />
-          <BiItalic
-            id="2"
-            onClick={on}
-            className={sty === "2" ? "cli" : "icon"}
-          />
-          <IoIosQuote
-            id="3"
-            onClick={on}
-            className={sty === "3" ? "cli" : "icon"}
-          />
-          <FiLink2
-            id="4"
-            onClick={on}
-            className={sty === "4" ? "cli" : "icon"}
-          />
-          <MdOutlineFormatListBulleted
-            onClick={on}
-            id="5"
-            className={sty === "5" ? "cli" : "icon"}
-          />
-          <MdOutlineFormatListNumbered
-            onClick={on}
-            id="6"
-            className={sty === "6" ? "cli" : "icon"}
-          />
-          <AiFillCamera
-            id="7"
-            onClick={on}
-            className={sty === "7" ? "cli" : "icon"}
-          />
-          <BsCodeSlash
-            id="8"
-            onClick={on}
-            className={sty === "8" ? "cli" : "icon"}
-          />
-          <BsBraces
-            id="9"
-            onClick={on}
-            className={sty === "9" ? "cli" : "icon"}
-          />
+
+        <div className="tag">
+          <button>
+            Tag 1 <MdOutlineCancel className="tagIcon" />
+          </button>
+          <button>
+            Tag 2 <MdOutlineCancel className="tagIcon" />
+          </button>
+          <button>
+            Tag 3 <MdOutlineCancel className="tagIcon" />
+          </button>
+        </div>
+        <div className="prev">
+          <img src="/prevv.png" alt="Preview Image" />
+        </div>
+
+        <div className="content">
+          <form action="">
+            <input type="text" placeholder="Title..." />
+
+            <textarea cols="30" placeholder="Description"></textarea>
+          </form>
+
+          <div className="ccancel" onClick={change}>
+            {open ? (
+              <FaTimes className="icon" />
+            ) : (
+              <GiHamburgerMenu className="icon" />
+            )}
+          </div>
+          <div className={open ? "active" : "het"}>
+            <BiBold
+              onClick={toggleBold}
+              className={properties.isBold ? "cli" : "icon"}
+            />
+            <BiItalic
+              onClick={toggleItalic}
+              className={properties.isItalic ? "cli" : "icon"}
+            />
+            <IoIosQuote
+              onClick={toggleQuote}
+              className={properties.isQuote ? "cli" : "icon"}
+            />
+            <FiLink2
+              onClick={toogleLink}
+              className={properties.isLink ? "cli" : "icon"}
+            />
+            <MdOutlineFormatListBulleted
+              onClick={bullet}
+              className={properties.isBullet ? "cli" : "icon"}
+            />
+            <MdOutlineFormatListNumbered
+              onClick={toggleList}
+              className={properties.isList ? "cli" : "icon"}
+            />
+            <AiFillCamera
+              onClick={pic}
+              className={properties.isPic ? "cli" : "icon"}
+            />
+            <BsCodeSlash
+              onClick={Code}
+              className={properties.isCode ? "cli" : "icon"}
+            />
+            <BsBraces
+              onClick={Bracket}
+              className={properties.isBraket ? "cli" : "icon"}
+            />
+          </div>
         </div>
       </div>
+      {/* <Poperties/>  */}
     </div>
   );
 };
